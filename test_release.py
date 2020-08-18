bcfef00d461805f9e88b1db0cf497e589b010e04
Matthew R Becker <becker.mr@gmail.com>
2020-08-18 07:06:52 -0500

    try this

--- test_release.py
+++ test_release.py
@@ -1,5 +1,7 @@
 import os
+import time
 import github
+import tqdm
 
 fname = "linux-64/cf-autotick-bot-test-package-0.9-py36h9f0ad1d_172.tar.bz2"
 
@@ -8,11 +10,14 @@ subdir, pkg = os.path.split(fname)
 gh = github.Github(os.environ["GITHUB_TOKEN"])
 
 repo = gh.get_repo("regro/releases")
-repo.create_repository_dispatch(
-    "release",
-    {
-        "subdir": subdir,
-        "package": pkg,
-        "repodata-shard": {"repodata": {"sha256": "blah"}},
-    }
-)
+
+for _ in tqdm.trange(10000):
+    repo.create_repository_dispatch(
+        "release",
+        {
+            "subdir": subdir,
+            "package": pkg,
+            "repodata-shard": {"repodata": {"sha256": "blah"}},
+        }
+    )
+    time.sleep(1)
