079ab2cf744cf5bff1c1ad2ac256f7f90c9ecf0c
Matthew R Becker <becker.mr@gmail.com>
2020-08-18 06:22:59 -0500

    ENH try this

---
+++ make_release.py
@@ -0,0 +1,53 @@
+import os
+import sys
+import json
+import tempfile
+import subprocess
+
+import github
+
+with open(os.environ["GITHUB_EVENT_PATH"], 'r') as fp:
+    event_data = json.load(fp)
+
+event_name = os.environ['GITHUB_EVENT_NAME'].lower()
+
+if event_name != "release":
+    sys.exit(1)
+
+
+gh = github.Github(os.environ["GITHUB_TOKEN"])
+repo = gh.get_repo("regro/releases")
+
+sha = subprocess.run(
+    "git rev-parse --verify HEAD",
+    shell=True,
+    capture_output=True,
+).stdout.decode("utf-8").strip()
+
+subdir = event_data['client_payload']["subdir"]
+pkg = event_data['client_payload']["package"]
+sha256 = event_data["client_payload"]["repodata-shard"]["repodata"]["sha256"]
+print("subdir/package: %s/%s" % (subdir, pkg), flush=True)
+print("sha256:", sha256, flush=True)
+
+url = f"https://conda.anaconda.org/conda-forge/{subdir}/{pkg}"
+
+with tempfile.TemporaryDirectory() as tmpdir:
+    subprocess.run(
+        f"curl -L {url} > {tmpdir}/{pkg}",
+        shell=True,
+    )
+
+    rel = repo.create_git_tag_and_release(
+        sha256,
+        "",
+        f"{subdir}/{pkg}",
+        "",
+        sha,
+        "commit",
+    )
+
+    rel.upload_asset(
+        f"{tmpdir}/{pkg}",
+        content_type="application/x-bzip2",
+    )
---
+++ test_release.py
@@ -0,0 +1,18 @@
+import os
+import github
+
+fname = "linux-64/cf-autotick-bot-test-package-0.9-py36h9f0ad1d_172.tar.bz2"
+
+subdir, pkg = os.path.split(fname)
+
+gh = github.Github(os.environ["GITHUB_TOKEN"])
+
+repo = gh.get_repo("regeo/releases")
+repo.create_repository_dispatch(
+    "release",
+    {
+        "subdir": subdir,
+        "package": pkg,
+        "repodata-shard": {"repodata": {"sha256": "blah"}},
+    }
+)
