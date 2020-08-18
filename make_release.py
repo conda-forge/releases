2c57d35baab1542a3b43ecfa4e75ef6165425322
Matthew R Becker <becker.mr@gmail.com>
2020-08-18 06:35:18 -0500

    faster

--- make_release.py
+++ make_release.py
@@ -2,6 +2,7 @@ import os
 import json
 import tempfile
 import subprocess
+import uuid
 
 import github
 
@@ -9,8 +10,6 @@ with open(os.environ["GITHUB_EVENT_PATH"], 'r') as fp:
     event_data = json.load(fp)
 
 event_name = os.environ['GITHUB_EVENT_NAME'].lower()
-
-print("event_name:", event_name, flush=True)
 
 gh = github.Github(os.environ["GITHUB_TOKEN"])
 repo = gh.get_repo("regro/releases")
@@ -25,9 +24,10 @@ subdir = event_data['client_payload']["subdir"]
 pkg = event_data['client_payload']["package"]
 sha256 = event_data["client_payload"]["repodata-shard"]["repodata"]["sha256"]
 print("subdir/package: %s/%s" % (subdir, pkg), flush=True)
-print("sha256:", sha256, flush=True)
 
 url = f"https://conda.anaconda.org/conda-forge/{subdir}/{pkg}"
+
+uid = uuid.uuid4().hex
 
 with tempfile.TemporaryDirectory() as tmpdir:
     subprocess.run(
@@ -36,7 +36,7 @@ with tempfile.TemporaryDirectory() as tmpdir:
     )
 
     rel = repo.create_git_tag_and_release(
-        sha256,
+        f"{uid}/{subdir}/{pkg}",
         "",
         f"{subdir}/{pkg}",
         "",
